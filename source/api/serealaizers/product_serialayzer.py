016e22953f80221b02d2ab5f617204112783e42d
Almas <kasper10.0711@gmail.com>
2024-03-30 12:37:34 +0500

    Сделал сериализацию продукта для апи

--- source/api/serealaizers/product_serialayzer.py
+++ source/api/serealaizers/product_serialayzer.py
@@ -1,5 +1,30 @@
 from rest_framework import serializers
 
+from source.market.models.product_model import ProductModel
 
-class Test(serializers.HyperlinkedModelSerializer):
-    pass
+
+
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ProductModel
+        fields = [
+            'id', 
+            'product_image', 
+            'product_name', 
+            'product_categories', 
+            'product_description',
+            'product_qty',
+            'product_price',
+            'is_deleted',
+            'created_at',
+            'updated_at',
+            'deleted_at'
+            ]
+        read_only_fields = [
+            'id', 
+            'is_deleted', 
+            'created_at', 
+            'updated_at', 
+            'deleted_at'
+            ]
